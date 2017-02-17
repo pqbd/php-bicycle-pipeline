f5dbe7dbca8d8b543403b7603c23b4427ed30933
avolkov <alx.volkov@gmail.com>
2017-02-17 11:04:12 +0300

    Initialize

---
+++ README.md
@@ -0,0 +1,9 @@
+#<NULL>
+the component for [alternative manifest](https://github.com/pqbd/php-bicycle "bicycle")
+
+<NULL> is ...
+
+## Code sample
+```php
+// code sample
+```
---
+++ tests/config.php
@@ -0,0 +1,7 @@
+<?php
+date_default_timezone_set( 'UTC');
+
+define( 'PUBLIC_DIR', __DIR__);
+define( 'ROOT_DIR', PUBLIC_DIR.'/..');
+
+require_once( ROOT_DIR.'/vendor/autoload.php');
